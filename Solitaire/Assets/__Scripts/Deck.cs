05e725023a57bf4f3b57b59370c8c3004535202a
Isfand <izzywhizzzz@gmail.com>
2019-05-29 14:31:04 +0100

    card pips are now showing correctly on each card sprite

--- Solitaire/Assets/__Scripts/Deck.cs
+++ Solitaire/Assets/__Scripts/Deck.cs
@@ -212,8 +212,87 @@ public class Deck : MonoBehaviour
         card.def = GetCardDefinitionByRank(card.rank);
 
         AddDecorators(card);
+        AddPips(card);
+        AddFace(card);
+
 
         return card;
+    }
+
+    private void AddFace(Card card)
+    {
+        if (card.def.face == "")
+        {
+            return; // No need to run if this isn't a face card
+        }
+
+        _tGO = Instantiate(prefabSprite) as GameObject;
+        _tSR = _tGO.GetComponent<SpriteRenderer>();
+        // Generate the right name and pass it to GetFace()
+        _tSp = GetFace(card.def.face + card.suit);
+        _tSR.sprite = _tSp; // Assign this Sprite to _tSR
+        _tSR.sortingOrder = 1; // set the sorting order
+        _tGO.transform.SetParent(card.transform);
+        _tGO.transform.localPosition = Vector3.zero;
+        _tGO.name = "face";
+    }
+
+    /// <summary>
+    /// Find the proper face card Sprite
+    /// </summary>
+    /// <returns>The face.</returns>
+    /// <param name="faceS">Face s.</param>
+    private Sprite GetFace(string faceS)
+    {
+        foreach (Sprite _tSP in faceSprites)
+        {
+            // If this Sprite has the right name..
+            if (_tSp.name == faceS)
+            {
+                // ... Then return the Sprite
+                return (_tSp);
+            }
+        }
+        // if nothing can be found return null
+        return null;
+    }
+
+    /// <summary>
+    /// Adds the pips ensuring the correct amount depending on the card.
+    /// </summary>
+    /// <param name="card">Card Object</param>
+    private void AddPips(Card card)
+    {
+        // For each of the pips in the definition
+        foreach (Decorator pip in card.def.pips)
+        {
+            // Instantiate a Sprite GameObject
+            _tGO = Instantiate(prefabSprite) as GameObject;
+            // Set the parent to be the card GameObject
+            _tGO.transform.SetParent(card.transform);
+            // Set the position to that specified in the XML
+            _tGO.transform.localPosition = pip.loc;
+            // Flip if necessary
+            if (pip.flip)
+            {
+                _tGO.transform.rotation = Quaternion.Euler(0, 0, 180);
+            }
+            // Scale if if necssart (only for the Ace)
+            if (pip.scale != 1)
+            {
+                _tGO.transform.localScale = Vector3.one * pip.scale;
+            }
+            // Give this GameObject a name
+            _tGO.name = "pip";
+            // Get the SpriteRender Component
+            _tSR = _tGO.GetComponent<SpriteRenderer>();
+            // Set the Sprite to the proper suit
+            _tSR.sprite = dictSuits[card.suit];
+            // Set sorting Order so the pip is rendered above the Card_Front
+            _tSR.sortingOrder = 1;
+            // Add this to the Cards list of pips
+            card.pipGOs.Add(_tGO);
+        }
     }
 
     // These private variables will be reused several times in helper methods
